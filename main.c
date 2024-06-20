511ea2944edf8b718261670dcc8729532f65719b
Jason Hilder <jhilder95@gmail.com>
2024-06-20 21:13:11 +0200

    Example of using lib.

--- main.c
+++ main.c
@@ -1,15 +1,34 @@
 #include "string_helper.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 
 int main() {
-    string_helper *s = create_string_helper("Hello");
+    string_helper *sh = create_string_helper("Hello");
 
-    print_string(s);
-    string_add(s, " World!", false);
-    print_string(s);
+    print_string(sh);
+    printf("String len: %d\n", (int)strlen(sh->value));
+    printf("Allocation len: %d\n", sh->allocated_size);
+    // ===============================================
 
-    destroy_string_helper(s);
+    string_add(sh, " World!", false);
+    print_string(sh);
+    printf("String len: %d\n", (int)strlen(sh->value));
+    printf("Allocation len: %d\n", sh->allocated_size);
+    // ===============================================
 
+    string_add(sh, "The ", true);
+    print_string(sh);
+    printf("String len: %d\n", (int)strlen(sh->value));
+    printf("Allocation len: %d\n", sh->allocated_size);
+    // ===============================================
+
+    string_replace(sh, "The ", "");
+    print_string(sh);
+    printf("String len: %d\n", (int)strlen(sh->value));
+    printf("Allocation len: %d\n", sh->allocated_size);
+    // ===============================================
+
+    destroy_string_helper(sh);
     return 0;
 }
